a64413daa04cccbb24ecb5ff07247d0f93230e08
abubakarzafar77 <abubakar.zafar@purelogics.net>
2017-10-22 13:23:24 -0700

    global objects

---
+++ global_objects.js
@@ -0,0 +1,12 @@
+var time = 0;
+setTimeout();
+var timer = setInterval(function () {
+    time += 2;
+    console.log(time + " seconds have passed !");
+    if (time > 5) {
+        clearInterval(timer);
+        console.log("serInterval closed! after 7");
+        console.log(__dirname);
+        console.log(__filename);
+    }
+}, 2000);
